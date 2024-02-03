747babd5bb3b39c4764872a016f89d8f8b38b7ef
infuriating <luca@infs.world>
2024-02-03 20:49:28 +0100

    Refactor project-work component

--- src/app/(pages)/(home)/components/ui/project-work.tsx
+++ src/app/(pages)/(home)/components/ui/project-work.tsx
@@ -12,15 +12,12 @@ export default function ProjectWork({
   description: string;
 }) {
   return (
-    <Link
-      href={`/projects/${slug}`}
-      className="group w-max text-muted-foreground"
-    >
+    <Link href={`/projects/${slug}`} className="group text-muted-foreground">
       <GradientText
         className="mb-1 text-xl font-bold brightness-200 saturate-0 transition-all group-hover:brightness-100 group-hover:saturate-100 lg:mb-2"
         text={title}
       />
-      <p className="mb-2 text-base transition-all group-hover:text-white lg:mb-0">
+      <p className="mb-2 w-full truncate text-base transition-all group-hover:text-white lg:mb-0">
         {description}
       </p>
     </Link>
