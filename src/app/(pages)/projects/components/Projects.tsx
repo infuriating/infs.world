664034c098b5ab1b0ee15e34097baf5dfa35c048
infuriating <luca@infs.world>
2024-02-10 23:12:19 +0100

    added tags to the project page with the option to filter them

--- src/app/(pages)/projects/components/Projects.tsx
+++ src/app/(pages)/projects/components/Projects.tsx
@@ -4,11 +4,14 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 import { Preloaded, usePreloadedQuery } from "convex/react";
 import { api } from "../../../../../convex/_generated/api";
+import Tags from "./Tags";
 
 export default function Projects(params: {
   preloadedProjects: Preloaded<typeof api.project.getAll>;
 }) {
   const projects = usePreloadedQuery(params.preloadedProjects);
+  const [selectedTags, setSelectedTags] = React.useState<string[]>([]);
+  const tags: string[] = [];
   if (!projects || projects.length === 0) return <>No projects found!</>;
 
   projects.sort((a, b) => {
@@ -17,10 +20,30 @@ export default function Projects(params: {
     );
   });
 
+  projects.forEach((project) => {
+    if (!project.tags) return;
+    project.tags.forEach((tag) => {
+      if (!tags.includes(tag)) {
+        tags.push(tag);
+      }
+    });
+  });
+
+  const filteredProjects = projects.filter((project) => {
+    if (selectedTags.length === 0) return true;
+    if (!project.tags) return;
+    return project.tags.some((tag) => selectedTags.includes(tag));
+  });
+
   return (
-    <div className="flex justify-center">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
-        {projects.map((project) => (
+    <div className="flex flex-col justify-center gap-y-6">
+      <Tags
+        tags={tags}
+        selectedTags={selectedTags}
+        setSelectedTags={setSelectedTags}
+      />
+      <div className="grid grid-cols-1 place-items-center gap-4 md:grid-cols-2 xl:grid-cols-3">
+        {filteredProjects.map((project) => (
           <ProjectItem
             key={project._id}
             image={project.images[0]}
---
+++ src/app/(pages)/projects/components/Tags.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import React, { useRef } from "react";
+
+export default function Tags({
+  tags,
+  selectedTags,
+  setSelectedTags,
+}: {
+  tags: string[];
+  selectedTags: string[];
+  setSelectedTags: React.Dispatch<React.SetStateAction<string[]>>;
+}) {
+  const tagDivRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div className="flex w-full flex-col overflow-x-auto px-8">
+      <h1 className="pb-2 text-xl font-medium">Filter by tags</h1>
+      {selectedTags.length > 0 && (
+        <Button
+          variant={"secondary"}
+          className="mb-2"
+          onClick={() => setSelectedTags([])}
+        >
+          Deselect All Tags
+        </Button>
+      )}
+
+      <div
+        className={`flex gap-2 overflow-x-auto
+        ${
+          // @ts-ignore
+          tagDivRef.current?.scrollWidth > tagDivRef.current?.clientWidth
+            ? "mx-4"
+            : ""
+        }
+        `}
+        style={{ flexShrink: 0 }}
+        ref={tagDivRef}
+      >
+        {/* @ts-ignore */}
+        {tagDivRef.current?.scrollWidth > tagDivRef.current?.clientWidth && (
+          <Button
+            className="absolute left-6 h-6"
+            size={"sm"}
+            variant={"secondary"}
+            onClick={() => {
+              tagDivRef.current?.scrollBy({ left: 150, behavior: "smooth" });
+            }}
+          >
+            {">"}
+          </Button>
+        )}
+        {tags.map((tag) => (
+          <Button
+            size={"sm"}
+            variant={selectedTags.includes(tag) ? "default" : "secondary"}
+            className="h-6"
+            onClick={() => {
+              if (selectedTags.includes(tag)) {
+                setSelectedTags(selectedTags.filter((t) => t !== tag));
+              } else {
+                setSelectedTags([...selectedTags, tag]);
+              }
+            }}
+            key={tag}
+          >
+            {tag}
+          </Button>
+        ))}
+        {/* @ts-ignore */}
+        {tagDivRef.current?.scrollWidth > tagDivRef.current?.clientWidth && (
+          <Button
+            className="absolute right-6 h-6"
+            size={"sm"}
+            variant={"secondary"}
+            onClick={() => {
+              tagDivRef.current?.scrollBy({ left: -150, behavior: "smooth" });
+            }}
+          >
+            {"<"}
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
