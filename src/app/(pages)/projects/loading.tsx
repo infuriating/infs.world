f49919df96e233a6cb0dfcd04ba017e8b8e23090
infuriating <luca@infs.world>
2024-02-10 23:17:50 +0100

    Updated loading state for /projects

--- src/app/(pages)/projects/loading.tsx
+++ src/app/(pages)/projects/loading.tsx
@@ -3,10 +3,25 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 
 const projects = Array.from({ length: 3 }, (_, i) => i);
+const tags = Array.from({ length: 24 }, (_, i) => i);
+const widths = ["w-4", "w-6", "w-8", "w-10", "w-12"];
 
 export default function loading() {
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col justify-center gap-y-6">
+      <div className="flex w-full flex-col overflow-x-auto px-8">
+        <h1 className="pb-2 text-xl font-medium">Filter by tags</h1>
+        <div className="flex gap-2 overflow-x-auto">
+          {tags.map((tag) => (
+            <Skeleton
+              key={tag}
+              className={`h-6 ${
+                widths[Math.floor(Math.random() * widths.length)]
+              }  rounded-full`}
+            />
+          ))}
+        </div>
+      </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
         {projects.map((project, i) => (
           <div className="group w-max rounded-lg border p-4" key={project}>
