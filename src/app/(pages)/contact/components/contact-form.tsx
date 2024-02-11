ee758ea4899eb76d887c718d7fafcba6fa81702b
infuriating <luca@infs.world>
2024-02-11 15:45:37 +0100

    added a new submit function with regex

--- src/app/(pages)/contact/components/contact-form.tsx
+++ src/app/(pages)/contact/components/contact-form.tsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import React, { useState } from "react";
+import { toast } from "sonner";
 
 export default function ContactForm({
   send,
@@ -17,15 +18,35 @@ export default function ContactForm({
     message: "",
   });
 
+  const submit = (data: { name: string; email: string; message: string }) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(data.email))
+      return toast.error("Please enter a valid email!");
+
+    if (data.name.length < 3) return toast.error("Please enter a valid name!");
+    if (data.email.length < 3)
+      return toast.error("Please enter a valid email!");
+    if (data.message.length < 3)
+      return toast.error("Please enter a valid message!");
+
+    if (data.name.length > 50) return toast.error("Name is too long!");
+    if (data.email.length > 40) return toast.error("Email is too long!");
+    if (data.message.length > 400)
+      return toast.error("Message is too long! Please shorten it!");
+
+    send(data);
+
+    toast.success(
+      `Hi ${data.name}! Your message has been sent! You'll receive a confirmation email shortly.`,
+    );
+  };
+
   return (
     <form
-      onSubmit={() =>
-        send({
-          name: formData.name,
-          email: formData.email,
-          message: formData.message,
-        })
-      }
+      onSubmit={(e) => {
+        e.preventDefault();
+        submit(formData);
+      }}
       className="flex flex-col gap-y-2"
     >
       <div className="flex flex-col gap-y-1">
