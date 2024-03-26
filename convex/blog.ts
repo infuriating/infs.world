86cdc6efb0a3369d517ca7b7005a8d0404667bf8
infuriating <luca@infs.world>
2024-03-26 14:06:50 +0100

    Add blog-related queries and handlers

---
+++ convex/blog.ts
@@ -0,0 +1,28 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const getAll = query({
+  handler: async (ctx) => {
+    return await ctx.db.query("blogPosts").collect();
+  },
+});
+
+export const getLatestTwo = query({
+  handler: async (ctx) => {
+    return await ctx.db.query("blogPosts").order("desc").take(2);
+  },
+});
+
+export const getBlogPost = query({
+  args: {
+    slug: v.string(),
+  },
+  handler: async (ctx, { slug }) => {
+    const project = await ctx.db
+      .query("blogPosts")
+      .filter((q) => q.eq(q.field("slug"), slug))
+      .first();
+
+    return project;
+  },
+});
