fa67c64a4cf8fabcf1cf761ab9fed932890cf7f3
infuriating <luca@infs.world>
2024-03-27 08:59:15 +0100

    quick fix - preloadQuery -> getAll instead of getLatestTwo

--- src/app/(pages)/blog/page.tsx
+++ src/app/(pages)/blog/page.tsx
@@ -3,6 +3,6 @@ import { api } from "../../../../convex/_generated/api";
 import BlogPosts from "./components/BlogPosts";
 
 export default async function BlogWrapper() {
-  const preloadedProjects = await preloadQuery(api.blog.getLatestTwo);
+  const preloadedProjects = await preloadQuery(api.blog.getAll);
   return <BlogPosts preloadedProjects={preloadedProjects} />;
 }
