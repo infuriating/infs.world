fb7e6c4f92a97bbe6ca583893dbc6cb7e307bc55
infuriating <luca@infs.world>
2024-03-27 08:59:59 +0100

    added cn utility for classname and delay as prop

--- src/components/ui/in-animation-wrapper.tsx
+++ src/components/ui/in-animation-wrapper.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import { motion, cubicBezier } from "framer-motion";
+import { cn } from "@/lib/utils";
 
 export default function InAnimationWrapper({
   children,
+  className,
+  delay,
 }: {
   children: React.ReactNode;
+  className?: string;
+  delay?: number;
 }) {
   return (
     <div className="overflow-hidden">
       <motion.div
-        initial={{ opacity: 0, y: "120%", scale: 0.925, filter: "blur(6px)" }}
+        initial={{ opacity: 0, y: "100%", scale: 0.925, filter: "blur(6px)" }}
         animate={{ opacity: 1, y: 0, scale: 1, filter: "blur(0px)" }}
         transition={{
-          delay: 3.1,
           duration: 1.2,
+          delay: delay || 0,
           ease: cubicBezier(0.22, 0.61, 0.36, 1),
         }}
-        className="flex justify-center"
+        className={cn("flex justify-center", className)}
       >
         {children}
       </motion.div>
