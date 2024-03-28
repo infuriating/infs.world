5a4569e175e8b415a19064321711b96610de8e77
infuriating <luca@infs.world>
2024-03-28 12:29:27 +0100

    Added mutation functions from cms repo

--- convex/blog.ts
+++ convex/blog.ts
@@ -1,4 +1,4 @@
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 export const getAll = query({
@@ -24,5 +24,80 @@ export const getBlogPost = query({
       .first();
 
     return project;
+  },
+});
+
+export const addBlogPost = mutation({
+  args: {
+    title: v.string(),
+    content: v.array(v.string()),
+    tags: v.array(v.string()),
+    image: v.string(),
+  },
+  handler: async (ctx, { title, content, tags, image }) => {
+    const slug = title
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9]/g, "")
+      .replace(/ /g, "-");
+    content = content[0].split("\n");
+    tags = tags[0].split(",");
+
+    const project = await ctx.db.insert("blogPosts", {
+      content,
+      slug,
+      tags,
+      title,
+      image,
+    });
+
+    return project;
+  },
+});
+
+export const updateBlogPost = mutation({
+  args: {
+    _id: v.string(),
+    title: v.string(),
+    content: v.array(v.string()),
+    tags: v.array(v.string()),
+    image: v.string(),
+  },
+  handler: async (ctx, { _id, title, content, tags, image }) => {
+    const blogPost = await ctx.db
+      .query("blogPosts")
+      .filter((q) => q.eq(q.field("_id"), _id))
+      .first();
+    if (!blogPost) throw new ConvexError("Project not found");
+
+    title ? (blogPost.title = title) : (blogPost.title = blogPost.title);
+    (blogPost.slug = title
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9]/g, "")
+      .replace(/ /g, "-")),
+      content
+        ? (blogPost.content = content)
+        : (blogPost.content = blogPost.content);
+    tags ? (blogPost.tags = tags) : (blogPost.tags = blogPost.tags);
+    image ? (blogPost.image = image) : (blogPost.image = blogPost.image);
+
+    content = content[0].split("\n");
+    tags = tags[0].split(",");
+
+    return await ctx.db.replace(blogPost._id, blogPost);
+  },
+});
+
+export const deleteBlogPost = mutation({
+  args: {
+    _id: v.string(),
+  },
+  handler: async (ctx, { _id }) => {
+    const blogPost = await ctx.db
+      .query("blogPosts")
+      .filter((q) => q.eq(q.field("_id"), _id))
+      .first();
+    if (!blogPost) throw new ConvexError("Project not found");
+
+    return await ctx.db.delete(blogPost._id);
   },
 });
--- convex/project.ts
+++ convex/project.ts
@@ -1,4 +1,4 @@
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 export const getAll = query({
@@ -24,5 +24,152 @@ export const getProject = query({
       .first();
 
     return project;
+  },
+});
+
+export const addProject = mutation({
+  args: {
+    title: v.string(),
+    description: v.string(),
+    images: v.array(v.string()),
+    inDevelopment: v.boolean(),
+    technologies: v.array(v.string()),
+    collaborators: v.array(
+      v.object({
+        name: v.string(),
+        role: v.string(),
+        github: v.string(),
+        linkedin: v.string(),
+        website: v.string(),
+      }),
+    ),
+    github: v.string(),
+    tags: v.array(v.string()),
+    website: v.string(),
+  },
+  handler: async (
+    ctx,
+    {
+      title,
+      description,
+      images,
+      inDevelopment,
+      technologies,
+      collaborators,
+      github,
+      tags,
+      website,
+    },
+  ) => {
+    const slug = title
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9]/g, "")
+      .replace(/ /g, "-");
+
+    technologies = technologies[0].split(",");
+    images = images[0].split(",");
+    tags = tags[0].split(",");
+
+    const project = await ctx.db.insert("projects", {
+      title,
+      description,
+      slug,
+      images,
+      inDevelopment,
+      technologies,
+      collaborators,
+      github,
+      tags,
+      website,
+    });
+
+    return project;
+  },
+});
+
+export const updateProject = mutation({
+  args: {
+    _id: v.string(),
+    title: v.string(),
+    description: v.string(),
+    images: v.array(v.string()),
+    inDevelopment: v.boolean(),
+    technologies: v.array(v.string()),
+    collaborators: v.array(
+      v.object({
+        name: v.string(),
+        role: v.string(),
+        github: v.string(),
+        linkedin: v.string(),
+        website: v.string(),
+      }),
+    ),
+    github: v.string(),
+    tags: v.array(v.string()),
+    website: v.string(),
+  },
+  handler: async (
+    ctx,
+    {
+      _id,
+      title,
+      description,
+      images,
+      inDevelopment,
+      technologies,
+      collaborators,
+      github,
+      tags,
+      website,
+    },
+  ) => {
+    const project = await ctx.db
+      .query("projects")
+      .filter((q) => q.eq(q.field("_id"), _id))
+      .first();
+    if (!project) throw new ConvexError("Project not found");
+
+    title ? (project.title = title) : (project.title = project.title);
+    description
+      ? (project.description = description)
+      : (project.description = project.description);
+    images ? (project.images = images) : (project.images = project.images);
+    inDevelopment
+      ? (project.inDevelopment = inDevelopment)
+      : (project.inDevelopment = project.inDevelopment);
+    technologies
+      ? (project.technologies = technologies)
+      : (project.technologies = project.technologies);
+    collaborators
+      ? (project.collaborators = collaborators)
+      : (project.collaborators = project.collaborators);
+    github ? (project.github = github) : (project.github = project.github);
+    tags ? (project.tags = tags) : (project.tags = project.tags);
+    website ? (project.website = website) : (project.website = project.website);
+
+    project.slug = title
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9]/g, "")
+      .replace(/ /g, "-");
+    project.technologies = project.technologies[0].split(",");
+    project.images = project.images[0].split(",");
+    project.tags && (project.tags = project.tags[0].split(","));
+
+    return await ctx.db.replace(project._id, project);
+  },
+});
+
+export const deleteProject = mutation({
+  args: {
+    _id: v.string(),
+  },
+  handler: async (ctx, { _id }) => {
+    const project = await ctx.db
+      .query("projects")
+      .filter((q) => q.eq(q.field("_id"), _id))
+      .first();
+    if (!project) throw new ConvexError("Project not found");
+
+    await ctx.db.delete(project._id);
   },
 });
