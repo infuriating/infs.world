9a5a7de75561386073dab28a56c67b95ae581284
infuriating <luca@infs.world>
2024-04-02 13:18:53 +0200

    Add Notification component to Page

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./header/Header";
 import Grain from "./Grain";
 import ReturnButton from "./ReturnButton";
+import Notification from "./header/Notification";
 
 export default function Page({
   children,
@@ -15,6 +16,7 @@ export default function Page({
           <div className="hidden aspect-square h-36 w-36 rounded-full bg-gradient-to-br from-fuchsia-800 to-transparent opacity-90 blur-[105px] lg:absolute lg:-bottom-14 lg:-left-14 lg:block"></div>
         </div>
       </div>
+      <Notification message="Check out my new blog!" nav={"/blog"} />
       <Header />
       <Grain />
 
