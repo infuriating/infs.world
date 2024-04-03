c0b8ae527ab636bddf2d42fc59eed4912a2e40c3
infuriating <luca@infs.world>
2024-04-03 11:41:30 +0200

    Fixed ScrollBar sticking to the viewport if scrolled to the right

--- src/app/(pages)/blog/[blogPost]/components/ScrollBar.tsx
+++ src/app/(pages)/blog/[blogPost]/components/ScrollBar.tsx
@@ -12,7 +12,7 @@ export default function BlogPostScrollBar() {
   });
 
   return (
-    <div className="pointer-events none fixed left-0 top-0 -z-10 h-full w-full">
+    <div className="pointer-events-none absolute -left-4 top-0 -z-10 h-full w-full">
       <motion.div
         style={{
           height: `${scrollY * 100}%`,
