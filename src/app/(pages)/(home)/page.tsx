afd7a1ecbaf019069a25abf1a58a56e58503ff34
infuriating <luca@infs.world>
2024-04-07 22:51:51 +0200

    Refactor component rendering in Home page

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -4,17 +4,21 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import WorkWrapper from "./components/work/WorkWrapper";
 import BlogWrapper from "./components/blog/BlogWrapper";
+import AllComponents from "./AllComponents";
 
 export default function Home() {
+  const components = [
+    <About key={2} />,
+    <Skills key={3} />,
+    <WorkWrapper key={4} />,
+    <BlogWrapper key={5} />,
+  ];
   return (
     <div className="relative flex min-h-[calc(100_-_104px)] w-full flex-col justify-between gap-16 pb-4">
       <main className="flex w-full flex-col justify-center">
         <div className="flex flex-col gap-12">
           <Introduction />
-          <About />
-          <Skills />
-          <WorkWrapper />
-          <BlogWrapper />
+          <AllComponents components={components} />
         </div>
       </main>
     </div>
