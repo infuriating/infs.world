673fc5aca1a3c71e229c72c673b96c7b6a8db1f9
infuriating <luca@infs.world>
2024-07-04 14:13:37 +0200

    edited nextConfig to allow images from all domains

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["utfs.io", "media.discordapp.net"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
